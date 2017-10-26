Cardinal-AOSP/vendor_cardinal

vendor: cardinal: Add in dialer_experience.xml to fix google phone bitching at us

common.mk
@@ -53,6 +53,11 @@ PRODUCT_COPY_FILES += \
     vendor/cardinal/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
     vendor/cardinal/prebuilt/common/bin/whitelist:system/addon.d/whitelist
 
+
+# Dialer fix
+PRODUCT_COPY_FILES +=  \
+    vendor/cardinal/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cardinal/overlay/dictionaries
 

Citrus-CAF/vendor_citrus

common.mk
@@ -59,6 +59,10 @@ PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 endif
 
+# Dialer fix
+PRODUCT_COPY_FILES +=  \
+    vendor/citrus/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/bin/sysinit:system/bin/sysinit \