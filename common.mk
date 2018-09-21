aac6325517d18a2d534ae230d87469caa8c105eb
Christopher N. Hesse <raymanfx@gmail.com>
2018-09-21 23:00:26 +0200

    vendor/citrus: common: Allow custom vendor to override properties
    Test case: override ro.adb.secure in vendor/extra/product.mk
    Before this patch, the override would not affect the resulting
    property.
    
    Change-Id: I0deb8a6865030b09ed6ba0ede1658319ca033e67
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- common.mk
+++ common.mk
@@ -1,3 +1,6 @@
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -216,9 +219,6 @@ PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
     vendor/citrus/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
-
-# include extra product makefile if exists
-$(call inherit-product-if-exists, vendor/extra/product.mk)
 
 # include sounds from pixel
 $(call inherit-product-if-exists, vendor/citrus/google/sounds/PixelSounds.mk)
